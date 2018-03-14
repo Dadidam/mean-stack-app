cabf52fc37c0b9851ef8d4e8e71d5aba6641488c
iliav <iliav@hurdit.com>
2018-03-13 23:07:17 -0400

    Added Passport JS and Google strategy for it

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecrent: keys.googleClientSecret,
+      callbackURL: 'auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // PORT from environment for production deployment
 // For development we use 4999 port number
