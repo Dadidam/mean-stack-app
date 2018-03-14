558040012d6368d58a05e990f3d402034150b4eb
iliav <iliav@hurdit.com>
2018-03-14 14:33:09 -0400

    Added Google passport handler

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ passport.use(
   )
 );
 
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
 // PORT from environment for production deployment
 // For development we use 4999 port number
 const PORT = process.env.PORT || 4999;
