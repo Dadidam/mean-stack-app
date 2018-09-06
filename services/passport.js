aeb8e4ac1fb1ac2447e6f628ec4a4039b557f5c9
Iliav <iliav@hurdit.com>
2018-09-06 19:13:27 -0400

    Using async/await at passport callback

--- services/passport.js
+++ services/passport.js
@@ -23,16 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      // we already have a record with the gicen profile ID
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      // we don't have a user record with this ID, make a new record!
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
