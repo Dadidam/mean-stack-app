115f90bd04f0c22312d99b91096b5c025acc9308
iliav <iliav@hurdit.com>
2018-11-28 15:24:18 -0500

    Extend payment component (provided name and description and some styles)

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -6,9 +6,13 @@ class Payments extends Component {
     return (
       <StripeCheckout
         amount={500}
+        name="Emaily Credits"
+        description="Add some credits to your account"
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
