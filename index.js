4fa4e2a429dc805f08a9ba79afd278ab7a30ba9f
Iliav <iliav@hurdit.com>
2019-02-07 11:00:48 -0500

    Added billing and client side app

--- index.js
+++ index.js
@@ -27,6 +27,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Epress will serve up production assests
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express wil serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // PORT from environment for production deployment
 // For development we use 4999 port number
 const PORT = process.env.PORT || 4999;
