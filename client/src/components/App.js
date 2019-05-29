43c8d8365ce06940389af822ce73ffab89ca24d3
Iliav <iliav@hurdit.com>
2019-05-29 10:36:32 -0400

    Survey form added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,8 +6,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-
-const Survey = () => <h2>Survey</h2>;
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
@@ -22,7 +21,7 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={Survey} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
